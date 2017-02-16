009eb35a79a4fd0bfdc2847ed00a5ad4dffcc5ec
Baek-soo-hyun <100suhyun@naver.com>
2017-02-16 17:59:31 +0900

    2017.02.16 업데이트

--- src/js/search.js
+++ src/js/search.js
@@ -25,11 +25,25 @@ require([
 		});
 	}
 
+	function scrollFilterLayerCss() {
+		$(".filter-layer").css({
+			"position": "fixed",
+			"top": "0px",
+		});
+	}
+	function initFilterLayerCss() {
+		$(".filter-layer").css({
+			"position": "absolute",
+			"top": "auto",
+		});
+	}
+
 	$(".click-filter").on("click", function() {
 		var select = $(this).children("div");
 
 		if ($(this).hasClass("filter")) {
 			$(select).toggleClass("");
+			showFilterlayer();
 		}
 		else {
 			$(select).toggleClass("active");
@@ -41,10 +55,52 @@ require([
 			var initPosition = $(this).scrollTop();
 			if(initPosition > 155) {
 				scrollSearchFilterCss();
+				scrollFilterLayerCss();
 			}
 			else {
 				initSearchFilterCss();
+				initFilterLayerCss();
 			}
 		});
 	});
+
+	/* var popupCssSelector = "";
+
+	function closeLayerPopup() {
+		$(popupCssSelector).hide();
+	}
+
+	function openLayerPopup(cssSelector) {
+		 if (popupCssSelector === cssSelector) {
+			return;
+		}
+
+		popupCssSelector = cssSelector;
+
+		$(cssSelector).show();
+
+		$(".submit-btn").on("click", function() {
+			closeLayerPopup();
+		});
+	}*/
+
+	function hideFilterlayer() {
+		$(".submit-btn").click(function() {
+			$(".filter-layer").animate({
+				width: 0,
+			}, "1500");
+			$("html, body").animate({
+				scrollTop: 0,
+			}, 400);
+		});
+	}
+
+	// 팝업레이어 slideLeft //
+	function showFilterlayer() {
+		var boxWidth = "380px";
+		$(".filter-layer").animate({
+			width: boxWidth,
+		}, "1500");
+		hideFilterlayer();
+	}
 });
